50a4437dff04060ec8f79c108742ee4819fd85dd
Farzana Mohamed <farzana.am@gadgeon.com>
2018-03-08 14:50:13 +0530

    Udemy_Sections 3,4,5,6

--- Section 3/ShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Section 3/ShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes:Recipe[] = [
+   new Recipe('A Test Recipe','This is simply a test','https://i1.wp.com/mypullzone.9vexd6dl53at.maxcdn-edge.com/wp-content/uploads/2016/08/kadai-paneer-gravy-recipe-step-by-step-instructions.jpg?resize=1024%2C915'),
+   new Recipe('A Test Recipe','This is simply a test','https://i1.wp.com/mypullzone.9vexd6dl53at.maxcdn-edge.com/wp-content/uploads/2016/08/kadai-paneer-gravy-recipe-step-by-step-instructions.jpg?resize=1024%2C915')
+   ];
   constructor() { }
 
   ngOnInit() {
